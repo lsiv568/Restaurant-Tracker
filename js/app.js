3b6d1915953870120fa7dbf1f2f519207f2f95af
Louis Sivillo <louissivillo@Louis-Sivillos-MacBook-Pro.local>
2013-02-23 16:57:31 -0500

    Started adding ability to display restaurants

--- js/app.js
+++ js/app.js
@@ -5,17 +5,20 @@
     'appSecret': '2128bcc817fd4ce0acb9da7d9c398c00'
   });
 
+  /** Handles displaying add restaurant form modal dialog */
   $("#addRestaurantModal").modal({show: false});
   $("#addRestaurant").click(function (event) {
       var idString = "#" + event.target.id + "Modal";
       $(idString).modal("show");
   })
 
+  /** Loads Restaurant.js entity via require and stores it in Restaurant */
   var Restaurant = {};
   require(["js/entities/Restaurant"], function(_Restaurant){
     Restaurant = _Restaurant;
   });
 
+  /*** Handles form submission */
   $("#new-restaurant-form").submit(function (event) {
     event.preventDefault();
     var form = $(this);
@@ -24,16 +27,32 @@
     var restaurant = new Restaurant(argument);
     $(details).each(function() {
       argument[this.name] = this.value;
-    })
+    });
     restaurant.save({
       success: function (resaurant) {
         alert("Saved restaurant!");
         form[0].reset();
       },
       error: function (error) {
-        alert("Unable to save restaurt due to " + error);
+        alert("Unable to save restaurt due to " + error.error + " " + error.description);
       }
     });
   });
-  
+
+  /** Load all data in Restaurant collection */
+  var restaurants = new Kinvey.Collection("restaurants");
+  restaurants.fetch({
+    success: function (listOfRestaurants) {
+      $(listOfRestaurants).each(function(index, restaurant) {
+        var theRestaurant = restaurant.attr;
+        var ul = "<ul>"
+        ul += "<li>" + "attendees: " + theRestaurant.attendees + "</li></ul>";
+        $("#visited-restaurants").append(ul);
+      });  
+    },
+    error: function (error) {
+      alert("Unable to retrieve restaurants due to " + error.error + " " + error.description);
+    }
+  });
+
 }(window, window.Kinvey));
--- js/entities/Restaurant.js
+++ js/entities/Restaurant.js
@@ -3,25 +3,25 @@ define([], function () {
     return Kinvey.Entity.extend({
         // Overwrite the constructor to automatically link all instances to the restaurants collection.
         constructor: function(attributes) {
-            Kinvey.Entity.prototype.constructor.call(this, attributes, 'restaurants');
+            Kinvey.Entity.prototype.constructor.call(this, attributes, "restaurants");
         },
         getName: function() {
-            return this.get('name');
+            return this.get("name");
         },
         getDate: function() {
-            return this.get('date');
+            return this.get("date");
         },
         getAttendees: function() {
-            return this.get('attendees');
+            return this.get("attendees");
         },
         getLocation: function() {
-          return this.get('location');
+          return this.get("location");
         },
         getRating: function() {
-          return this.get('rating');
+          return this.get("rating");
         },
         getNotes: function() {
-          return this.get('notes');
+          return this.get("notes");
         }
     });
 
