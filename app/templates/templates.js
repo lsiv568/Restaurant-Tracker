66130df98d430fd86c478901de3725c41c9c6be4
Louis Sivillo <louissivillo@new-host-2.home>
2013-02-24 16:54:06 -0500

    Began basic styling

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -6,7 +6,7 @@ helpers = helpers || Handlebars.helpers; data = data || {};
   var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression;
 
 
-  buffer += "<div>\n  <div>\n    <span><strong>Name</strong>:</span>\n    <span>";
+  buffer += "<div class=\"restaurant-entry\">\n  <div>\n    <span><strong>Name</strong>:</span>\n    <span>";
   if (stack1 = helpers.name) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.name; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
